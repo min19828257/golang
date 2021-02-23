b1311a3db0c80dbe1f2f4c21865e64cdf4e5346b
kimminsu <minsukim@databiz.or.kr>
2021-02-23 11:04:14 +0900

    create AES

---
+++ hashEncryto/AES/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"crypto/aes"
+	"fmt"
+)
+
+func main() {
+	key := "Hello, Key 12345" // 16 바이트
+	s := "Hello, world! 12"   // 16 바이트
+
+	block, err := aes.NewCipher([]byte(key)) //AES 대칭키 암호화 블록 생성
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ciphertext := make([]byte, len(s))
+	block.Encrypt(ciphertext, []byte(s))
+	fmt.Printf("%x\n", ciphertext)
+
+	plaintext := make([]byte, len(s))
+	block.Decrypt(plaintext, ciphertext) // AES 알고리즘으로 암호화된 데이터를 평문으로 복호화
+
+	fmt.Println(string(plaintext))
+}
